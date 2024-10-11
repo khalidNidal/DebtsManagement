69e66f560a29ea7eb282c2c974ed028f7db0d8e5
Khalid Nidal <khalidnidal541@gmail.com>
2024-10-11 20:27:09 +0300

    start-up and create the entites

---
+++ DebtsMangment.Coree/Entities/Debts.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DebtsMangment.Core.Entities
+{
+    public class Debts
+    {
+       public int Id { get; set; }
+        public int CustomerId { get; set; }
+        public string MedicineName { get; set; }
+        public double Price { get; set; }
+        public DateTime DateAdded { get; set; }
+
+
+
+}
+}
